22372b17a4bc3fd68ba34173da775897ae254dc2
shanthosh04 <133089176+shanthosh04@users.noreply.github.com>
2023-09-18 11:59:34 +0200

    add cy.get

--- cypress/e2e/1-getting-started/test.cy.js
+++ cypress/e2e/1-getting-started/test.cy.js
@@ -1,12 +1,8 @@
-describe("Meine erste Test Suite", () => {
-    it("Testfall 1: Besuche eine Website und überprüfe den Titel", () => {
+describe("Erste Test Suite", () => {
+    it("Eine Website öfnnen", () => {
       cy.visit("https://www.google.com");
-      cy.title().should("eq", "Beispiel Domain");
+      cy.get("#L2AGLb").click();
     });
-  
-    it("Testfall 2: Überprüfe das Vorhandensein eines Elements", () => {
-      cy.visit("https://www.google.com/binladen");
-      cy.get("h1").should("exist");
-    });
+
   });
   
